fbb95c96ac4d1e5f11fb5c06e80d376335ae043f
EC2 Default User <ec2-user@ip-172-31-18-226.us-west-2.compute.internal>
2020-01-30 00:27:37 +0000

    initialized svg circle for the timer

---
+++ timer.js
@@ -0,0 +1,42 @@
+class Timer {
+    constructor(durationInput, startButton, pauseButton, callbacks) {
+        this.durationInput = durationInput;
+        this.startButton = startButton;
+        this.pauseButton = pauseButton;
+        
+        this.startButton.addEventListener('click', this.start);
+        this.pauseButton.addEventListener('click', this.pause);
+        
+        if (callbacks) {
+            this.onStart = callbacks.onStart;
+            this.onTick = callbacks.onTick;
+            this.onComplete = callbacks.onComplete;
+        };
+    };
+    
+    start = () => {
+        if (this.onStart) this.onStart();
+        this.tick();
+        this.intervalId = setInterval(this.tick, 1000);
+    };
+    
+    pause = () => clearInterval(this.intervalId);
+    
+    tick = () => {
+        if (this.timeRemaining <= 0) {
+            this.pause()
+            if (this.onComplete) this.onComplete();
+        } else {
+            this.timeRemaining = this.timeRemaining - 1;
+            if (this.onTick) this.onTick();
+        };
+    };
+    
+    get timeRemaining() {
+        return parseFloat(this.durationInput.value);
+    };
+    
+    set timeRemaining(time) {
+        this.durationInput.value = time;
+    };
+}
